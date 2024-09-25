12229a4a1e17dc3cf9e6780e4bedf4ac2954cb02
Leandro Lucarella <luca-frequenz@llucax.com>
2024-09-25 11:56:43 +0200

    Set `polling_interval` according to when changes are expected
    When testing for events in a file watcher, we need to set the
    `polling_interval` to a value that is small enough to detect the changes
    we are expecting, otherwise some events could be "merged" if the changes
    happen faster than the polling interval.
    
    Signed-off-by: Leandro Lucarella <luca-frequenz@llucax.com>

--- tests/test_file_watcher_integration.py
+++ tests/test_file_watcher_integration.py
@@ -26,7 +26,9 @@ async def test_file_watcher(tmp_path: pathlib.Path) -> None:
     number_of_writes = 0
     expected_number_of_writes = 3
 
-    file_watcher = FileWatcher(paths=[str(tmp_path)])
+    file_watcher = FileWatcher(
+        paths=[str(tmp_path)], polling_interval=timedelta(seconds=0.05)
+    )
     timer = Timer(timedelta(seconds=0.1), SkipMissedAndDrift())
 
     async for selected in select(file_watcher, timer):
@@ -65,6 +67,7 @@ async def test_file_watcher_deletes(tmp_path: pathlib.Path) -> None:
         paths=[str(tmp_path)],
         event_types={EventType.DELETE},
         force_polling=False,
+        polling_interval=timedelta(seconds=0.05),
     )
     write_timer = Timer(timedelta(seconds=0.1), SkipMissedAndDrift())
     deletion_timer = Timer(timedelta(seconds=0.25), SkipMissedAndDrift())
